bc544dcf728720d448f499bb6591aeefdeee376c
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2021-09-27 22:21:53 -0500

    GAAAAAA

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(Collider))]
+public class ClickHandler : MonoBehaviour{
+    public UnityEvent upEvent;
+    public UnityEvent downEvent;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Down");
+        downEvent?.Invoke();
+    }
+    void OnMouseUp()
+    {
+        //transform.Translate(-100, 0, 0);
+        Debug.Log("Up");
+        upEvent?.Invoke();
+    }
+}
