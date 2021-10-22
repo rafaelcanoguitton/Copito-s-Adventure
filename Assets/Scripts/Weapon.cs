db1aa52cb05561850ce7329418f7234b9ea19e2d
Rafael Isaac Cano Guitton <rafael.cano@ucsp.edu.pe>
2021-10-22 11:59:48 -0500

    Adding first level final version

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -49,6 +49,7 @@ public class Weapon : MonoBehaviour
         GameObject intantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
         Rigidbody bulletRigid = intantBullet.GetComponent<Rigidbody>();
         bulletRigid.velocity = bulletPos.forward * 50;
+
         yield return null;
         //2
         GameObject intantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
